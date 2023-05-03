4158c6c016bd48b381e317e9286c1c8015c7bdf3
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-03 23:50:35 +0500

    Add files via upload

---
+++ 01_linked_list.py
@@ -0,0 +1,29 @@
+class node:
+    __data   = None
+    __nxtptr = None
+    def __init__(self, val, nxt):
+        self.__data    = val
+        self.__nxtptr = nxt
+
+    def setval(self, val):
+        self.__data = val
+    
+    def setnxt(self, nxt):
+        self.__nxtptr = nxt
+
+    def getnxt(self):
+        return self.__nxtptr
+    
+    def getval(self):
+        return self.__data
+    
+
+def insert_at_start():
+    pass
+
+
+head = node()
+
+for _ in range(5):
+    insert_at_start(head)
+insert_at_start(head, 7)

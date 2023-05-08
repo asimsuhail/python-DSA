f44d4bd769ab8fd7de9bd5837d7f4f356a6fe161
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-08 22:59:54 +0500

    Update 01_linked_list.py

--- 01_linked_list.py
+++ 01_linked_list.py
@@ -2,9 +2,9 @@
 class node:
     __data   = None
     __nxtptr = None
-    def __init__(self, val, nxt):
-        self.__data    = val
-        self.__nxtptr = nxt
+    def __init__(self):
+        self.__data    = None
+        self.__nxtptr  = None
 
     def setval(self, val):
         self.__data = val
@@ -19,12 +19,27 @@ class node:
         return self.__data
     
 
-def insert_at_start():
-    pass
+def insert_at_start(h, val):
+    temp = node()
+    temp.setnxt(None)
+    temp.setval(val)
+    if h == None:
+        h = temp
+    else:
+        temp.setnxt(h)
+        h = temp
+    return h
 
+def display(h):
+    cur = node
+    cur = h
+    while cur != None:
+        print(cur.getval())
+        cur = cur.getnxt()
 
-head = node()
+head = None
 
 for _ in range(5):
-    insert_at_start(head)
-insert_at_start(head, 7)
+    head = insert_at_start(head, 5)
+
+display(head)

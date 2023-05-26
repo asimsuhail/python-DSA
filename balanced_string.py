eea87b005eff7f7ed15dc7227205bc1dac04952d
Asim Suhail <71958833+asimsuhail@users.noreply.github.com>
2023-05-26 23:54:38 +0500

    Update balanced_string.py

--- balanced_string.py
+++ balanced_string.py
@@ -6,13 +6,12 @@ str = list(str)
 li = []
 
 for i in str:
-    if i == '(' or i == '{' or i == '[':
+    if i in [ '(', '{', '[' ]:
         li.append(i)
         print(li)
-    if i == ')' or i == '}' or i == ']':
-        if li[len(li) - 1] == i:
-            li.pop()
-            print(li)
+    if (i == ')' and li[len(li) - 1] == '(' ) or (i == '}' and li[len(li) - 1] == '{' ) or (i == ']' and li[len(li) - 1] == '[' ):
+        li.pop()
+        print(li)
 if len(li) == 0:
     print("balanced")
 else:
